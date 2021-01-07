74ef8128f7d2e7cc8dd54125a0f3e8d3dbe34276
metacryst <samrussell99@gmail.com>
2021-01-06 20:43:25 -0600

    COMING SOON

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h2>SAM RUSSELL</h2>
-      <h2 className="Metacryst">METACRYST</h2>
+      <h2 className="Metacryst">COMING SOON</h2>
       <div className="line"></div>
     </div>
   );
