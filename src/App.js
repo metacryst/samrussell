af5b33cf56fd0a899ca4b5e0de1927ba2a400f35
metacryst <samrussell99@gmail.com>
2021-01-06 20:38:03 -0600

    new endeavors

--- src/App.js
+++ src/App.js
@@ -3,9 +3,37 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>SAM RUSSELL</h2>
-      <h2 className="Metacryst">METACRYST</h2>
-      <div className="line"></div>
+      <h1 className="dsig">ΔΣΦ</h1>
+      
+
+
+      <h1 className="parchment">"harvard art"</h1>
+      <h1 className="parchment">paperclip//</h1>
+      <h1 className="parchment">[wip]</h1>
+      <h1 className="parchment">parchment</h1>
+      
+      
+      <h1 className="book">Who Will Survive in America</h1>
+      <h1 className="book">Make America Great Again</h1>
+      
+      
+      <h1 className="song">I saw my angels on a parking garage</h1>
+      <h1 className="song">A Letter</h1>
+      <h1 className="song">Zeus</h1>
+      
+      
+      <h1 className="class">Physics I</h1>
+      <h1 className="class">Multivariable Calculus</h1>
+      <h1 className="class">Linear Algebra</h1>
+      <h1 className="class">Discrete Structures</h1>
+      
+      
+      
+      
+      
+      <div className="line1"></div>
+      <div className="line2"></div>
+      <div className="timeLine"></div>
     </div>
   );
 }
