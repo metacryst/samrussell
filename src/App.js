58073d58c9d22bf4ff511679f5a1c65e50d5c0f5
metacryst <samrussell99@gmail.com>
2021-01-31 13:35:46 -0800

    business card

--- src/App.js
+++ src/App.js
@@ -1,39 +1,76 @@
 import './App.css';
+import { useHistory, Route } from 'react-router-dom';
+
 
 function App() {
+  
+  const history = useHistory();
+
+  for(let i = 0; i<14; i++) {
+    
+  }
+  
   return (
     <div className="App">
-      <h1 className="dsig">ΔΣΦ</h1>
+      
+      <h2 className="name"> Sam Russell </h2>
+      
+      
+      <h1 className="zero" onClick={() => {
+        history.push('/dsig');
+      }}>
+        softwareCPR
+      </h1>
+
+      <h1 className="one" onClick={() => {
+        history.push('/dsig');
+      }}>
+        ΔΣΦ
+      </h1>
       
 
-
-      <h1 className="parchment">"harvard art"</h1>
-      <h1 className="parchment">paperclip//</h1>
-      <h1 className="parchment">[wip]</h1>
-      <h1 className="parchment">parchment</h1>
+      <h1 className="two" onClick={() => {
+        window.location.href = 'http://harvard--art.herokuapp.com/';
+      }}>
+        "harvard art"
+      </h1>
+      
+      <h1 className="three" onClick={() => {
+        window.location.href = 'http://paperclip--house.herokuapp.com/';
+      }}>
+        paperclip//
+      </h1>
+      
+      <h1 className="four" onClick={() => {
+        window.location.href = 'http://wip--gallery.herokuapp.com/';
+      }}>
+        [wip]
+      </h1>
+      
+      <h1 className="five">~parchment</h1>
       
       
-      <h1 className="book">Who Will Survive in America</h1>
-      <h1 className="book">Make America Great Again</h1>
+      <h1 className="six">Who Will Survive in America</h1>
+      <h1 className="seven">Make America Great Again</h1>
       
       
-      <h1 className="song">I saw my angels on a parking garage</h1>
-      <h1 className="song">A Letter</h1>
-      <h1 className="song">Zeus</h1>
+      <h1 className="eight">I saw my angels on a parking garage</h1>
+      <h1 className="nine">A Letter</h1>
+      <h1 className="ten">Zeus</h1>
       
       
-      <h1 className="class">Physics I</h1>
-      <h1 className="class">Multivariable Calculus</h1>
-      <h1 className="class">Linear Algebra</h1>
-      <h1 className="class">Discrete Structures</h1>
+      <h1 className="eleven">Physics I</h1>
+      <h1 className="twelve">Multivariable Calculus</h1>
+      <h1 className="thirteen">Linear Algebra</h1>
+      <h1 className="fourteen">Discrete Structures</h1>
       
       
       
       
-      
+{/*       
       <div className="line1"></div>
       <div className="line2"></div>
-      <div className="timeLine"></div>
+      <div className="timeLine"></div> */}
     </div>
   );
 }
