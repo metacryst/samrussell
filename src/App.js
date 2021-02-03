21418d9efdd58dd65b1faf2cada73a28da577050
metacryst <samrussell99@gmail.com>
2021-02-02 23:02:09 -0800

    finished fraternity & work links

--- src/App.js
+++ src/App.js
@@ -1,68 +1,152 @@
 import './App.css';
-import { useHistory, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useHistory, Route, Link } from 'react-router-dom';
 
 
 function App() {
   
+  const [background, setBackground] = useState("")
+  
   const history = useHistory();
-
-  for(let i = 0; i<14; i++) {
-    
-  }
+  
   
   return (
     <div className="App">
       
-      <h2 className="name"> Sam Russell </h2>
+      <Route
+					path='/softwareCPR'
+					exact={true}
+					render={() => {
+						return (
+							<div className="softwareCPRWrapper">
+                <h2 className="name" className="softwareCPRName" onClick={() => {
+                  history.push('/')
+                }}> 
+                  Sam Russell 
+                </h2>
+								<h2 className="softwareCPRInfo" onClick={() => {;
+                }}>
+									: Software Testing Engineer for
+								</h2>
+                <h2 className="softwareCPRLink" onClick={() => {
+                  window.location.href = 'https://softwarecpr.com/';
+                }}>
+									softwareCPR
+								</h2>
+							</div>
+						);
+					}}
+				/>
       
       
-      <h1 className="zero" onClick={() => {
-        history.push('/dsig');
-      }}>
-        softwareCPR
-      </h1>
+      <Route
+					path='/dsig'
+					exact={true}
+					render={() => {
+						return (
+							<div className="dsigWrapper">
+                <h2 className="dsigName" onClick={() => {
+                  history.push('/')
+                }}> 
+                  Sam Russell 
+                </h2>
+								<div className="dsigInfo">
+									coming soon.
+								</div>
+							</div>
+						);
+					}}
+				/>
+        
+        
+        <Route
+					path='/parchment'
+					exact={true}
+					render={() => {
+						return (
+							<div className="wrapper">
+                <h2 className="name" onClick={() => {
+                  history.push('/')
+                }}> 
+                  Sam Russell 
+                </h2>
+								<div className="parchmentInfo">
+									coming soon.
+								</div>
+							</div>
+						);
+					}}
+				/>
+        
+      
+      <Route
+					path='/'
+					exact={true}
+					render={() => {
+						return (
+							<>      
+                
+                <h2 className="name" onClick={() => {
+                  history.push('/')
+                  setBackground("#f8eadd")
+                }}> 
+                  Sam Russell 
+                </h2>
+                
+                <h1 className="zero" onClick={() => {
+                  history.push('/softwareCPR');
+                }}>
+                  softwareCPR
+                </h1>
 
-      <h1 className="one" onClick={() => {
-        history.push('/dsig');
-      }}>
-        ΔΣΦ
-      </h1>
-      
+                <h1 className="one" onClick={() => {
+                  history.push('/dsig');
+                }}>
+                  ΔΣΦ
+                </h1>
+                
 
-      <h1 className="two" onClick={() => {
-        window.location.href = 'http://harvard--art.herokuapp.com/';
-      }}>
-        "harvard art"
-      </h1>
+                <h1 className="two" onClick={() => {
+                  window.location.href = 'http://harvard--art.herokuapp.com/';
+                }}>
+                  "harvard art"
+                </h1>
+                
+                <h1 className="three" onClick={() => {
+                  window.location.href = 'http://paperclip--house.herokuapp.com/';
+                }}>
+                  paperclip//
+                </h1>
+                
+                <h1 className="four" onClick={() => {
+                  window.location.href = 'http://wip--gallery.herokuapp.com/';
+                }}>
+                  [wip]
+                </h1>
+                
+                <h1 className="five"onClick={() => {
+                  history.push('/parchment');
+                }}>~parchment</h1>
+                
+                
+                <h1 className="six">Who Will Survive in America</h1>
+                <h1 className="seven">Make America Great Again</h1>
+                
+                
+                <h1 className="eight">I saw my angels on a parking garage</h1>
+                <h1 className="nine">A Letter</h1>
+                <h1 className="ten">Zeus</h1>
+                
+                
+                <h1 className="eleven">Physics I</h1>
+                <h1 className="twelve">Multivariable Calculus</h1>
+                <h1 className="thirteen">Linear Algebra</h1>
+                <h1 className="fourteen">Discrete Structures</h1>
+							</>
+						);
+					}}
+				/>
       
-      <h1 className="three" onClick={() => {
-        window.location.href = 'http://paperclip--house.herokuapp.com/';
-      }}>
-        paperclip//
-      </h1>
-      
-      <h1 className="four" onClick={() => {
-        window.location.href = 'http://wip--gallery.herokuapp.com/';
-      }}>
-        [wip]
-      </h1>
-      
-      <h1 className="five">~parchment</h1>
-      
-      
-      <h1 className="six">Who Will Survive in America</h1>
-      <h1 className="seven">Make America Great Again</h1>
-      
-      
-      <h1 className="eight">I saw my angels on a parking garage</h1>
-      <h1 className="nine">A Letter</h1>
-      <h1 className="ten">Zeus</h1>
-      
-      
-      <h1 className="eleven">Physics I</h1>
-      <h1 className="twelve">Multivariable Calculus</h1>
-      <h1 className="thirteen">Linear Algebra</h1>
-      <h1 className="fourteen">Discrete Structures</h1>
       
       
       
