529f54b0358241a993eab02250f8969a913189d8
metacryst <samrussell99@gmail.com>
2021-03-11 15:41:49 -0600

    finished class screens, some iphone things

--- src/App.js
+++ src/App.js
@@ -336,6 +336,12 @@ function App() {
                 }}> 
                   Sam Russell 
                 </h2>
+                <h1 className="email">
+                  sam@samrussell.com
+                </h1>
+                
+                
+                
                 
                 <h1 className="zero" onClick={() => {
                   history.push('/softwareCPR');
