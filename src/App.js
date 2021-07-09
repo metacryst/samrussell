f99c6e337cc7da09de768dfdc70f3732388800b2
metacryst <samrussell99@gmail.com>
2021-07-09 15:16:07 -0500

    Changed API Key name

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   const [error, setError] = useState('')
   
   function getartData() {    
-    const url = `https://api.harvardartmuseums.org/object?classification=Paintings&sort=random&size=8&hasimage=1&apikey=${process.env.REACT_APP_KEY}`
+    const url = `https://api.harvardartmuseums.org/object?classification=Paintings&sort=random&size=8&hasimage=1&apikey=${process.env.HARVARD_ART_KEY}`
         
     fetch(url)
       .then(response => response.json())
